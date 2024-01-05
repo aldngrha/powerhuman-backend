d574d683fa812e97a67e8cd02cc954a7f6537867
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2024-01-05 14:09:13 +0700

    CRUD Role

---
+++ app/Http/Controllers/API/RoleController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Helpers\ResponseFormatter;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RoleRequest;
+use App\Models\Role;
+use Illuminate\Http\Request;
+use Exception;
+use Illuminate\Support\Facades\Auth;
+
+class RoleController extends Controller
+{
+    public function fetch(Request $request)
+    {
+        $id = $request->input('id');
+        $limit = $request->input('limit', 10);
+        $name = $request->input('name');
+
+        $roleQuery = Role::query();
+
+        if ($id) {
+            $role = $roleQuery->find($id);
+
+            if ($role) {
+                return ResponseFormatter::success($role, "Role found");
+            }
+
+            return ResponseFormatter::error('Role not found', 404);
+        }
+
+        $roles = $roleQuery->where("company_id", $request->company_id);
+
+        if ($name) {
+            $roles->where('name', 'like', '%' . $name . '%');
+        }
+
+        return ResponseFormatter::success($roles->paginate($limit), 'Role found');
+    }
+
+    public function create(RoleRequest $request)
+    {
+        try {
+
+            //create role
+            $role = Role::create([
+                "name" => $request->name,
+                "company_id" => $request->company_id
+            ]);
+
+            if (!$role) {
+                throw new Exception("Role not created");
+            }
+
+            return ResponseFormatter::success($role, "Role created");
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 500);
+        }
+    }
+
+    public function update(RoleRequest $request, $id) {
+        try {
+            $role = Role::find($id);
+
+            if(!$role) {
+                throw new Exception("Role not found");
+            }
+
+            $role->update([
+                "name" => $request->name,
+                "company_id" => $request->company_id,
+            ]);
+
+            return ResponseFormatter::success($role, "Role updated");
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 500);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $role = Role::find($id);
+
+            if (!$role) {
+                throw new Exception("Role not found");
+            }
+
+            $role->delete();
+
+            return ResponseFormatter::success(null, "Role deleted");
+        } catch (Exception $e) {
+            return ResponseFormatter::error($e->getMessage(), 500);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\API\UserController;
 use App\Http\Controllers\API\CompanyController;
 use App\Http\Controllers\API\TeamController;
+use App\Http\Controllers\API\RoleController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -34,6 +35,15 @@ Route::prefix("team")->middleware("auth:sanctum")->name("team.")->group(function
     Route::post('', [TeamController::class, "create"])->name("create");
     Route::post('/update/{id}', [TeamController::class, "update"])->name("update");
     Route::delete('/delete/{id}', [TeamController::class, "delete"])->name("delete");
+});
+
+
+// Role API
+Route::prefix("role")->middleware("auth:sanctum")->name("role.")->group(function () {
+    Route::get('', [RoleController::class, 'fetch'])->name("fetch");
+    Route::post('', [RoleController::class, "create"])->name("create");
+    Route::post('/update/{id}', [RoleController::class, "update"])->name("update");
+    Route::delete('/delete/{id}', [RoleController::class, "delete"])->name("delete");
 });
 
 // Auth API
